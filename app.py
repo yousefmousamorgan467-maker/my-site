60df8b579e5cfaad9048792c22849fb49c6ef9e8
yousefmousamorgan467-maker <yousefmousamorgan467@gmail.com>
2026-03-25 15:14:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,20 +9,19 @@ def home():
         <head>
             <title>Yousef Dev</title>
             <style>
-                body { background-color: #121212; color: #00ff00; text-align: center; font-family: sans-serif; padding-top: 50px; }
+                body { background-color: #1a1a1a; color: white; text-align: center; font-family: Arial; padding-top: 50px; }
                 h1 { color: #ff00ff; }
                 .btn { background: #ff00ff; color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px; }
             </style>
         </head>
         <body>
-            <h1>Welcome to Yousef's World! 🚀</h1>
-            <p>مبرمج مصري في تالتة ثانوي بيحب البايثون والمونتاج.</p>
+            <h1>Welcome to Yousef's World!</h1>
+            <p>يوسف في تالتة ثانوي بيحب البايثون والمونتاج</p>
             <br>
-            <a href="#" class="btn">قريباً: مذكرات الفيزياء والكيمياء</a>
+            <a href="#" class="btn">Physics & Chemistry</a>
         </body>
     </html>
     """
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
+# ده السطر المهم لـ Vercel
+# مش محتاجين app.run هنا لأنه هو اللي بيشغله
